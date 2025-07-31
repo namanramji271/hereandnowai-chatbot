4a4777d4fbd3474905706f3b1f1c497f5bef2623
4373 1162 <43731162@SCASA4.COM>
2025-07-31 12:54:17 +0530

    My first langchain bot

---
+++ Project-5-hello-langchain/hello_langchain.py
@@ -0,0 +1,20 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+google_api_key=os.getenv("GEMINI_API_KEY")
+model= "gemini-2.5-flash-lite"
+
+def run_hello_langchain():
+    """
+    Instantiate a ChatGoogleGenerativeAI model.
+    Invoke it with a simple prompt
+    Prints the model's response
+
+    """
+    llm=ChatGoogleGenerativeAI(model=model,google_api_key=google_api_key)
+    response = llm.invoke("Explain RLHF in 2 lines")
+    print(response.content)
+
+run_hello_langchain()
